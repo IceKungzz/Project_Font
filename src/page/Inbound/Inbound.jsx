e28cece4373bb5eaa6710fa33711efbaf468c9df
kop2911 <kopduangsuwan@gmail.com>
2024-12-12 21:57:46 +0700

    kop

--- src/page/Inbound/Inbound.jsx
+++ src/page/Inbound/Inbound.jsx
@@ -7,12 +7,10 @@ export function Inbound(props) {
         { title: 'ชื่อไซต์งาน:', type: "text" },
         { title: 'ที่อยู่ลูกค้า:', type: "text" },
         { title: 'วันที่เริ่มเช่า-ขาย:', type: "date", },
-
-
     ];
 
     return (
-        <div className='w-full h-[90%] bg-slate-800'>
+        <div className='w-full h-[90%] '>
 
             <div className='w-full h-full grid grid-cols-5'>
 
@@ -70,37 +68,114 @@ export function Inbound(props) {
                 </div>
 
 
-                <div className=" col-span-3 grid grid-rows-6  bg-red-600">
+                <div className=" col-span-3 grid grid-rows-6  ">
 
-                    <div className='row-span-5 grid  grid-rows-4 border border-gray-500 bg-slate-400 mt-2 rounded-lg'>
-                        <div className='row-span-1 grid  grid-cols-3 bg-orange-500'>
-                            <div className=' col-span-1 grid grid-rows-2 bg-gray-500'>
-                                <span>2</span>
-                                <span>2</span>
-                            </div>
-                            <div className=' col-span-1 bg-gray-600'>
+                    <div className='row-span-5 grid  grid-rows-4 border border-gray-500  mt-2 rounded-lg'>
 
-                            </div>
-                            <div className=' col-span-1 bg-gray-700'>
-
-                            </div>
+                        <div className='row-span-1 grid grid-cols-3 grid-rows-6  pl-4 pr-4 pt-2'>
+                            <span className='col-span-1  grid justify-start items-center'>ห้างหุ้นส่วนจำกัด ภัทรชัย แบบเหล็ก</span>
+                            <span className='col-span-1 row-span-2  grid justify-center items-center'>รายการส่งออกสินค้า</span>
+                            <span className='col-span-1 '></span>
+                            <span className='col-span-1  grid justify-start items-center'>สาขา: ชลบุรี</span>
+                            <span className='col-span-1  grid justify-end items-center'>12 ธันวาคม 2567</span>
+                            <span className='col-span-3  grid justify-start items-center'>นามลูกค้า/ชื่อบริษัท:
+                                คุณพรทิพย์</span>
+                            <span className='col-span-1  grid justify-start items-center'>ชื่อไซต์งาน: โรง2</span>
+                            <span className='col-span-1  grid justify-end items-center'>เริ่มเช่า: 27 พ.ย. 57</span>
+                            <span className='col-span-1  grid justify-end items-center'>สิ้นสุดเช่า: 03 ธ.ค.67</span>
+                            <span className='col-span-2 row-span-2 grid grid-cols-7'>
+                                <span className='col-span-1'>ที่อยู่ลูกค้า:</span>
+                                <span className='col-span-6'>บริษัท 123 จำกัด เลขที่ 22/11 หมู่ 1 ถนนลาดยาว ตำบลสองเสน อำเภอศรีราชา จังหวัดชลบุรี</span>
+                            </span>
+                            <span className='col-span-1  grid justify-end items-center'>ระยะเวลาเช่า: 7 วัน</span>
                         </div>
-                        <div className='row-span-3 grid grid-rows-3 bg-orange-400'>
-                            <div className='row-span-2 bg-orange-100'>
 
+                        <div className='row-span-2 bg-slate-400 grid grid-rows-3'>
+                            <div className=' row-span-3 overflow-auto  no-scrollbar border-b-4 flex justify-center items-start mr-3 ml-3'>
+                                <table className="w-full table-auto text-center border-collapse border-t-2">
+                                    <thead className="font-bold  bg-white sticky top-0 border-b-2">
+                                        <tr>
+                                            <th className="px-4 py-2">ลำดับ</th>
+                                            <th className="px-4 py-2">รายการ</th>
+                                            <th className="px-4 py-2">ขนาด</th>
+                                            <th className="px-4 py-2">รูปแบบ</th>
+                                            <th className="px-4 py-2">จำนวน</th>
+                                            <th className="px-4 py-2">ราคา</th>
+                                            <th className="px-4 py-2">รวม</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr className="border-b-2">
+                                            <td className="px-4 py-2">1</td>
+                                            <td className="px-4 py-2">M04 แบบเสาสี่เหลี่ยม</td>
+                                            <td className="px-4 py-2">100*100</td>
+                                            <td className="px-4 py-2">
+                                                <select
+                                                    name="model"
+                                                    id=""
+                                                    className="px-4 py-2 text-center "
+                                                >
+                                                    <option value="">เช่า</option>
+                                                    <option value="">ซื้อ</option>
+                                                </select>
+                                            </td>
+                                            <td className="px-4 py-2">
+                                                <input type="number" className="px-2 py-2 text-center " />
+                                            </td>
+                                            <td className="px-4 py-2 ">2,000.00</td>
+                                            <td className="px-4 py-2">4,000.00</td>
+                                        </tr>
+                                        <tr className="border-b">
+                                            <td className="px-4 py-2">2</td>
+                                            <td className="px-4 py-2">D10 แบบคาน</td>
+                                            <td className="px-4 py-2">10*100</td>
+                                            <td className="px-4 py-2">
+                                                <select
+                                                    name="model"
+                                                    id=""
+                                                    className="px-4 py-2 text-center "
+                                                >
+                                                    <option value="">เช่า</option>
+                                                    <option value="">ซื้อ</option>
+                                                </select>
+                                            </td>
+                                            <td className="px-4 py-2">
+                                                <input type="number" className="px-2 py-2 text-center" />
+                                            </td>
+                                            <td className="px-4 py-2">3,000.00</td>
+                                            <td className="px-4 py-2">9,000.00</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
-                            <div className='row-span-2 bg-orange-700'>
 
-                            </div>
+                            
+                        </div>
+
+                        <div className='row-span-1 grid grid-cols-6 grid-rows-3 bg-teal-100'>
+                                <span className="col-span-3 row-span-3 grid grid-cols-5 bg-red-100 pt-2">
+                                    <span className='col-span-3 grid justify-end'>
+                                      รวมรายการสินค้าที่ส่งออกทั้งหมด  
+                                    </span>
+                                    <span className='col-span-1 grid justify-center'>
+                                      1 
+                                    </span>
+                                    <span className='col-span-1 grid justify-start'>
+                                      รายการ
+                                    </span>
+                                </span>
+                                <span className='col-span-1'>ราคารวม</span>
+                                <span className='col-span-1'>120000</span>
+                                <span className='col-span-1'>บาท</span>
+                                <span className='col-span-1'>ราคารวม</span>
+                                <span className='col-span-1'>120000</span>
+                                <span className='col-span-1'>บาท</span>
+                                <span className='col-span-1'>ราคารวม</span>
+                                <span className='col-span-1'>120000</span>
+                                <span className='col-span-1'>บาท</span>
                         </div>
 
                     </div>
-
-
-
-
-
-
 
                     <div className='row-span-1  bg-slate-600'>
 
