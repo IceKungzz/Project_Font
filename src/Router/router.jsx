25b267013d585e3c303f101d9ea39e4e158ad0d7
IceKungzz <tanagit11213@hotmail.com>
2024-12-13 20:56:58 +0700

    Modal Outbound+ preorder 80%

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -2,12 +2,12 @@ import { createBrowserRouter } from "react-router-dom"
 import App from "../App"
 import Login from "../page/Authentication/Login"
 import PrivateRoute from "../Components/ComponentPrivateRoute/PrivateRoute"
-import Sidebar from "../Components/Layout_Components/Sidebar"
 import { Outbound } from "../page/Outbound/Outbound"
 import {Inbound} from "../page/Inbound/Inbound"
 import { ReturnItem } from "../page/Returnbound/Returnbound"
 import { Home } from "../page/Home/Home"
 import Preorder from "../page/Preorder/Preorder"
+import { Modal_Outbound } from "../page/Outbound/Modal_Outbound"
 
 
 const router = createBrowserRouter([
@@ -28,7 +28,7 @@ const router = createBrowserRouter([
         ],
     },
     {path:"/login", element: <Login/>},
-    {path:"/sidebar", element: <Sidebar/>},
+    {path:"/modal_outbound", element: <Modal_Outbound/>},
 ])
 
 export default router
