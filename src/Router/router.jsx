bcb4617805c9d6e2d9b7fcdceef78f222bae5760
meathane28 <nanmeathane28@gmail.com>
2024-12-15 21:33:32 +0700

    update_status_nan-01

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -9,6 +9,7 @@ import { Home } from "../page/Home/Home"
 import Preorder from "../page/Preorder/Preorder"
 import { Modal_Outbound } from "../page/Outbound/Modal_Outbound"
 import Modal_Outbound_SweetAlert from "../page/Outbound/Modal_oreder_SweetAlert"
+import { Status_product } from "../page/status/status"
 
 
 const router = createBrowserRouter([
@@ -23,6 +24,7 @@ const router = createBrowserRouter([
                     {path:"inbound", element: <Inbound/>},
                     {path:"", element: <Home/>},
                     {path:"returnitem", element: <ReturnItem/>},
+                    {path:"status", element: <Status_product/>},
                 ],
             },
             {path:"preorder", element: <Preorder/>},
